2fa69ef194bec1f2ade44e24886f5cb9be371329
emanuelsdrock@gmail.com <emanuelsdrock@gmail.com@b148248f-3baf-7d3c-e074-fc4faf7cfb04>
2011-06-28 15:40:13 +0000

    Classe de pesquisa por twetts

---
+++ jaytter/src/models/SearchTweets.java
@@ -0,0 +1,40 @@
+
+
+package models;
+
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Tweet;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+import java.util.List;
+
+/**
+ *
+ * @author emanuel
+ */
+public class SearchTweets {
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("java twitter4j.examples.search.SearchTweets [query]");
+            System.exit(-1);
+        }
+        Twitter twitter = new TwitterFactory().getInstance();
+        try {
+            QueryResult result = twitter.search(new Query(args[0]));
+            List<Tweet> tweets = result.getTweets();
+            for (Tweet tweet : tweets) {
+                System.out.println("@" + tweet.getFromUser() + " - " + tweet.getText());
+            }
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Failed to search tweets: " + te.getMessage());
+            System.exit(-1);
+        }
+    }
+
+}
