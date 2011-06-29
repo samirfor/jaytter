925bb80a450224bdae0673608e5947b541d246f4
emanuelsdrock@gmail.com <emanuelsdrock@gmail.com@b148248f-3baf-7d3c-e074-fc4faf7cfb04>
2011-06-29 01:38:49 +0000

    Classe para buscar por usuarios Não OK

--- jaytter/src/models/SearchUsers.java
+++ jaytter/src/models/SearchUsers.java
@@ -16,10 +16,40 @@
  */
 package models;
 
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Tweet;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+
+import java.util.List;
+import twitter4j.ResponseList;
+import twitter4j.User;
+
 /**
  *
  * @author emanuel
  */
 public class SearchUsers {
-    
+
+        public static void main (String args[]){
+        
+    String search = "EmanuelDomingos";
+
+        Twitter twitter = new TwitterFactory().getInstance();
+        try {
+            QueryResult result = (QueryResult) twitter.searchUsers(search, 1);
+            List<Tweet> tweets = result.getTweets();
+            for (Tweet tweet : tweets) {
+                System.out.println("@" + tweet.getFromUser() + " - " + tweet.getText());
+            }
+            System.exit(0);
+        } catch (TwitterException te) {
+            te.printStackTrace();
+            System.out.println("Falha na busca por tweets: " + te.getMessage());
+            System.exit(-1);
+        }
+    }
+
 }
